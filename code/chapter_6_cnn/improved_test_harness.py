aa1d20f55f83fc1e9a002ca0fad7b39c7f3bb928
Drew Barnett <programs@seattlego.org>
2019-05-15 23:49:50 -0700

    Improved pyplot outputs, changed one model, and minor bugfixes.

--- code/chapter_6_cnn/improved_test_harness.py
+++ code/chapter_6_cnn/improved_test_harness.py
@@ -38,52 +38,59 @@ model_0.summary()
 model_0.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
 # end:: one_layer_81_mlp_model[]
 
-# tag::one_layer_300_mlp_model[]
+# tag::one_layer_700_mlp_model[]
 model_1 = Sequential()
-model_1.add(Dense(300, activation='sigmoid', input_shape=(board_size,)))
+model_1.add(Dense(700, activation='sigmoid', input_shape=(board_size,)))
 model_1.add(Dense(board_size, activation='sigmoid'))
 model_1.summary()
 model_1.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
-# end::one_layer_300_mlp_model[]
-
-# tag::one_layer_700_mlp_model[]
-model_2 = Sequential()
-model_2.add(Dense(700, activation='sigmoid', input_shape=(board_size,)))
-model_2.add(Dense(board_size, activation='sigmoid'))
-model_2.summary()
-model_2.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
 # end::one_layer_700_mlp_model[]
 
 # tag:: one_layer_2100_mlp_model[]
-model_3 = Sequential()
-model_3.add(Dense(2100, activation='sigmoid', input_shape=(board_size,)))
-model_3.add(Dense(board_size, activation='sigmoid'))
-model_3.summary()
-model_3.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
+model_2 = Sequential()
+model_2.add(Dense(2100, activation='sigmoid', input_shape=(board_size,)))
+model_2.add(Dense(board_size, activation='sigmoid'))
+model_2.summary()
+model_2.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
 # end:: one_layer_2100_mlp_model[]
 
 # tag::three_layer_700_mlp_model[]
-model_4 = Sequential()
-model_4.add(Dense(200, activation='sigmoid', input_shape=(board_size,)))
-model_4.add(Dense(300, activation='sigmoid'))
-model_4.add(Dense(200, activation='sigmoid'))
-model_4.add(Dense(board_size, activation='sigmoid'))
-model_4.summary()
-model_4.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
+model_3 = Sequential()
+model_3.add(Dense(200, activation='sigmoid', input_shape=(board_size,)))
+model_3.add(Dense(300, activation='sigmoid'))
+model_3.add(Dense(200, activation='sigmoid'))
+model_3.add(Dense(board_size, activation='sigmoid'))
+model_3.summary()
+model_3.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
 # end:: three_layer_700_mlp_model[]
 
 # tag:: three_layer_224_cnn_model[]
-model_5 = Sequential()
-model_5.add(Conv2D(32, (3, 3), activation='sigmoid', input_shape=(size, size, 1)))
-model_5.add(Conv2D(64, (3, 3), activation='sigmoid'))
-model_5.add(Flatten())
-model_5.add(Dense(128, activation='sigmoid'))
-model_5.add(Dense(size**2, activation='sigmoid'))
-model_5.summary()
-model_5.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
+model_4 = Sequential()
+model_4.add(Conv2D(32, (3, 3), activation='sigmoid', input_shape=(size, size, 1)))
+model_4.add(Conv2D(64, (3, 3), activation='sigmoid'))
+model_4.add(Flatten())
+model_4.add(Dense(128, activation='sigmoid'))
+model_4.add(Dense(size**2, activation='sigmoid'))
+model_4.summary()
+model_4.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
 # end:: three_layer_224_cnn_model[]
 
-# tag:: four_layer_224_cnn_model[]
+# tag:: four_layer_224_cnn_mse_model[]
+model_5 = Sequential()
+model_5.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(size, size, 1)))
+model_5.add(Dropout(0.6))
+model_5.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model_5.add(MaxPooling2D((2, 2)))
+model_5.add(Dropout(0.6))
+model_5.add(Flatten())
+model_5.add(Dense(128, activation='relu'))
+model_5.add(Dropout(0.6))
+model_5.add(Dense(size**2, activation='softmax'))
+model_5.summary()
+model_5.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
+# end:: four_layer_224_cnn_mse_model[]
+
+# tag:: four_layer_224_cnn_cce_model[]
 model_6 = Sequential()
 model_6.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(size, size, 1)))
 model_6.add(Dropout(0.6))
@@ -95,27 +102,28 @@ model_6.add(Dense(128, activation='relu'))
 model_6.add(Dropout(0.6))
 model_6.add(Dense(size**2, activation='softmax'))
 model_6.summary()
-model_6.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
-# end:: four_layer_224_cnn_model[]
+model_6.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+# end:: four_layer_224_cnn_cce_model[]
 
 # start_tests
 m = 7
 model = [model_0, model_1, model_2, model_3, model_4, model_5, model_6]
+colors = ['k', 'b', 'g', 'r', 'c', 'm', 'y']
 names = [
-    '81n/1_mlp',
-    '300n/1_mlp',
-    '700n/1_mlp',
-    '2100n/1_mlp',
-    '700n/3_mlp',
-    '224n/2_cnn',
-    '224n/3_cnn']
-e = 1000
+    '81ns-1_mlp',
+    '700n-1_mlp',
+    '2100n-1_mlp',
+    '700n-3_mlp',
+    '224n-2_cnn',
+    '224n-3_cnn_mse',
+    '224n-3_cnn_cce']
+e = 2**13
 callback_times = TimeHistory()
 model_histories = [{} for x in range(m)]
 model_times = [[] for x in range(m)]
 
 for i in range(m):
-    if i < 5:
+    if i < 4:
         model_histories[i] = model[i].fit(
             X_train,
             Y_train,
@@ -141,18 +149,25 @@ file2 = 'test_history.png'
 
 fig = plt.figure()
 for i in range(m):
-    plt.loglog(model_times[i], model_histories[i].history['loss'])
-plt.title('MSE Loss / Time')
-plt.ylabel('MSE Loss')
+    if i == 6:
+        model_history = [x / 2.5 for x in model_histories[i].history['loss']]
+        plt.loglog(model_times[i], model_history, colors[i])
+    else:
+        model_history = [x * 100 for x in model_histories[i].history['loss']]
+        plt.loglog(model_times[i], model_history, colors[i])
+plt.title('Loss / Time')
+plt.ylabel('Loss')
 plt.xlabel('Seconds')
 names_2 = [x + '_loss' for x in names]
 plt.legend(names_2, bbox_to_anchor=(1.04,0.5), loc='center left')
-fig.savefig(file1, dpi=fig.dpi, bbox_inches='tight')
+fig.savefig(file1, dpi=250, bbox_inches='tight')
 
 fig = plt.figure()
 for i in range(m):
-    plt.semilogx(callback_times.times, model_histories[i].history['acc'])
-    plt.semilogx(callback_times.times, model_histories[i].history['val_acc'])
+    model_history = [x * 100 for x in model_histories[i].history['acc']]
+    plt.plot(callback_times.times, model_history, colors[i] + '-')
+    model_history = [x * 100 for x in model_histories[i].history['val_acc']]
+    plt.plot(callback_times.times, model_history, colors[i] + '--')
 plt.title('Test % / Time')
 plt.ylabel('Test %')
 plt.xlabel('Seconds')
@@ -163,5 +178,5 @@ for n in range(len(names_2)):
     else:
         names_2[n] = names[math.floor((n-1)/2)] + '_valid_acc'
 plt.legend(names_2, bbox_to_anchor=(1.04,0.5), loc='center left')
-fig.savefig(file2, dpi=fig.dpi, bbox_inches='tight')
+fig.savefig(file2, dpi=250, bbox_inches='tight')
 # end_tests
