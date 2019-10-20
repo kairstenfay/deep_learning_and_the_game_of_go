653eec68ac1602ff4130773d7b17d98b16f0971e
Drew Barnett <programs@seattlego.org>
2019-10-19 19:26:24 -0700

    Predict bot created, doesnt serve correctly yet.

---
+++ code/examples/serve_bot.py
@@ -0,0 +1,21 @@
+# Include the path to the local version of dlgo.
+import os
+import sys
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.join(dir_path, '..'))
+
+# tag::e2e_imports[]
+import h5py
+
+from dlgo.agent.predict import DeepLearningAgent, load_prediction_agent
+from dlgo.httpfrontend import get_web_app
+# end::e2e_imports[]
+
+# tag::e2e_load_agent[]
+model_file = h5py.File("../agents/deep_bot.h5", "r")
+bot_from_file = load_prediction_agent(model_file)
+model_file.close()
+
+web_app = get_web_app({'predict': bot_from_file})
+web_app.run()
+# end::e2e_load_agent[]
